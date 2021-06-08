c4da6177a51bc010fdd5f4a3a8b2c6751b812388
java1jmogo <java1jmogo@student.21-school.ru>
2021-06-08 10:44:50 +0300

    ex00 is finished

---
+++ ex00/srcs/megaphone.cpp
@@ -0,0 +1,21 @@
+#include "../includes/megaphone.hpp"
+
+int	main(int argc, char **argv)
+{
+	int	len;
+
+	len = 0;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			len = strlen(argv[i]);
+			for (int j = 0; j < len; j++)
+				std::cout << (char)toupper(argv[i][j]);
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
