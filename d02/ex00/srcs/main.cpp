f368ae25698a8ac22c9be55dcea9753448c30bf4
Goshiq <vangog-2005@ngs.ru>
2021-07-23 15:48:41 +0300

    ex01 is done

--- d02/ex00/srcs/main.cpp
+++ d02/ex00/srcs/main.cpp
@@ -5,12 +5,11 @@ int	main(int argc, char **argv)
 	(void)argc;
 	(void)argv;
 
-	Zombie	*tt;
 	Zombie	stackZombie = Zombie("A Stack Zombie!");
+	Zombie	*tt = newZombie("A Heap Zombie");
 
-	tt = newZombie("A Heap Zombie");
 	tt->announce();
 	stackZombie.announce();
 	randomChump("A Random Zombie");
-	delete tt;
+	delete (tt);
 }
