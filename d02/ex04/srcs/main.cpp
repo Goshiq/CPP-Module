7a3bb83c16bdb99607ee9472da6a171a68181cff
Goshiq <vangog-2005@ngs.ru>
2021-07-27 13:24:38 +0300

    we are opening a file now

--- d02/ex04/srcs/main.cpp
+++ d02/ex04/srcs/main.cpp
@@ -1,8 +1,17 @@
+#include <fstream>
 #include <iomanip>
+#include <iostream>
+#include <stdlib.h>
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	std::ifstream	inputFile;
 
+	if (argc != 4)
+		exit(1);
+	inputFile.open(argv[1]);
+	if (inputFile.is_open())
+		std::cout << "File OK" << std::endl;
+	else
+		std::cout << "NO FILE" << std::endl;
 }
